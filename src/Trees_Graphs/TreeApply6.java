f3ff894473e43c8ed616ca50de1b5df9dc285d0e
twingtwing <soyoungdev.7@gmail.com>
2022-09-29 00:00:31 +0900

    220929_Commit

---
+++ src/Trees_Graphs/TreeApply6.java
@@ -0,0 +1,15 @@
+package Trees_Graphs;
+/**
+ * 응용 : 배열의 값을 왼쪽부터 하나씩 넣으면서 이진검색트리를 만들었다.
+ *       주어진 이진검색트리를 만들 수 있는 모든 배열을 찾는 알고리즘 구현
+ *       (단, 트리에 중복값업음)
+ */
+class Trees6{
+
+}
+
+public class TreeApply6 {
+    public static void main(String[] args) {
+
+    }
+}
