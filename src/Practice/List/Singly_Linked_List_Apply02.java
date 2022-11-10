3eea187d196d78bdbbcb63d2d7b60d22e858b94e
김소영 <soyoungdev.7@gmail.com>
2022-11-11 01:58:32 +0900

    221111_Commit

--- src/Practice/List/Singly_Linked_List_Apply02.java
+++ src/Practice/List/Singly_Linked_List_Apply02.java
@@ -1,5 +1,7 @@
 package Practice.List;
 
+import com.sun.xml.internal.bind.marshaller.NoEscapeHandler;
+
 import java.util.Random;
 
 /*
@@ -71,7 +73,17 @@ class SinglyLinkedListApply2{
         System.out.println(num);
     }
 
-    // 응용 1.1 loop
+    void retrieveNumRev(){
+        Node n = header.link;
+        String num = "";
+        while(n.link != null){
+            num = num + n.data;
+            n = n.link;
+        }
+        num = num + n.data;
+        System.out.println(num);
+    }
+
     void appendNum(int data){
         Node next = this.header;
         int num = data;
@@ -83,6 +95,7 @@ class SinglyLinkedListApply2{
         }
     }
 
+    // 응용 1.1 loop
     void sumList(Node first, Node second){
         Node next = this.header;
         int sum = 0;
@@ -105,6 +118,93 @@ class SinglyLinkedListApply2{
         }
     }
 
+    // 응용 1.2 재귀호출
+    void sumListRec(Node first, Node second){
+        this.header.link = sumListRec(first,second,0);
+    }
+
+    Node sumListRec(Node first, Node second,int up){
+        if (first == null && second == null && up == 0) return null;
+        int one = 0, two = 0;
+        if (first != null){
+            one = first.data;
+            first = first.link;
+        }
+        if (second != null){
+            two = second.data;
+            second = second.link;
+        }
+        int sum = one + two + up;
+        Node result = new Node(sum%10);
+        Node node = sumListRec(first,second,sum >= 10 ? 1 : 0);
+        if (node != null){
+            result.link = node;
+        }
+        return result;
+    }
+
+    // 응용 2.1 재귀호출 + 저장소
+    class Storage{
+        int up;
+        Node node = null;
+    }
+
+    void sumRevRec(Node first, Node second){
+        this.header.link = sumRevRec(first,second, new Storage());
+    }
+
+    Node sumRevRec(Node first, Node second, Storage storage){
+        if (first == null && second == null && storage.up == 0) return null;
+        int one = 0, two = 0;
+        if (first != null){
+            one = first.data;
+            first = first.link;
+        }
+        if (second != null){
+            two = second.data;
+            second = second.link;
+        }
+        int sum = one + two + storage.up;
+        Node result = new Node(sum%10);
+        storage.up = sum >= 10 ? 1 : 0;
+
+        Node node = sumRevRec(first,second,storage);
+        if (storage.node != null){
+            storage.node.link = result;
+            storage.node = result;
+            return node;
+        }else{
+            storage.node = result;
+            return result;
+        }
+    }
+
+    // 응용 2.1.2
+
+    // 응용 2.2 합계를 구한 뒤에 새로운 LinkedList를 만듦
+    void sumListTotal(Node first, Node second){
+        if (first == null && second == null) return;
+        int one = sumTotal(first);
+        int two = sumTotal(second);
+        this.header.link = madeList(one + two);
+    }
+
+    int sumTotal(Node node){
+        if (node == null) return 0;
+        return sumTotal(node.link)*10 + node.data;
+    }
+
+    Node madeList(int num){
+        if (num == 0) return null;
+        while (num > 0){
+
+        }
+        return null;
+    }
+
+    // 응용 2.2.2
+
+
 }
 
 public class Singly_Linked_List_Apply02 {
@@ -113,6 +213,7 @@ public class Singly_Linked_List_Apply02 {
         int num02 = new Random().nextInt(1000000);
         System.out.println(num01);
         System.out.println(num02);
+        System.out.println(num01  + num02);
 
         SinglyLinkedListApply2 list01 = new SinglyLinkedListApply2();
         list01.appendNum(num01);
@@ -124,10 +225,25 @@ public class Singly_Linked_List_Apply02 {
         list02.retrieve();
         list02.retrieveNum();
 
+        // 응용 1.1 loop
         SinglyLinkedListApply2 list03 = new SinglyLinkedListApply2();
         list03.sumList(list01.getFirst(),list02.getFirst());
         list03.retrieve();
         list03.retrieveNum();
+
+        // 응용 1.2 재귀호출
+        SinglyLinkedListApply2 list05 = new SinglyLinkedListApply2();
+        list05.sumListRec(list01.getFirst(),list02.getFirst());
+        list05.retrieve();
+        list05.retrieveNum();
+
+        System.out.println();
+
+        // 응용 2.1 재귀호출 + 저장소
+        SinglyLinkedListApply2 list06 = new SinglyLinkedListApply2();
+        list06.sumRevRec(list01.getFirst(),list02.getFirst());
+        list06.retrieve();
+        list06.retrieveNumRev();
 
     }
 }
