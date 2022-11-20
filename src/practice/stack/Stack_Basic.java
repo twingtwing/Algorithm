5e3ff53788c659a3438677285880b32371138e7a
김소영 <soyoungdev.7@gmail.com>
2022-11-20 18:49:09 +0900

    221120_Commit

--- src/practice/stack/Stack_Basic.java
+++ src/practice/stack/Stack_Basic.java
@@ -79,13 +79,61 @@ class StackFixArray{
 
     int getIndex(int partIndex){
         if (partIndex >= part) throw new IndexOutOfBoundsException();
-        return partIndex * len + this.top[partIndex] + 1;
+        return partIndex * len + this.top[partIndex];
     }
 
     void push(int partIndex, int data) throws FullStackException{
         if (isFull(partIndex)) throw new FullStackException();
-        int index = partIndex * len + this.top[partIndex] + 1;
-        this.stackAry[index] = data;
+        this.stackAry[getIndex(partIndex) + 1] = data;
+        this.top[partIndex]++;
+    }
+
+    int pop(int partIndex){
+        if (isEmpty(partIndex)) throw new EmptyStackException();
+        int result = this.stackAry[getIndex(partIndex)];
+        this.top[partIndex]--;
+        return result;
+    }
+
+    int peek(int partIndex){
+        if (isEmpty(partIndex)) throw new EmptyStackException();
+        return this.stackAry[getIndex(partIndex)];
+    }
+
+    void retireve(){
+        for (int i = 0; i < this.part; i++){
+            if (isEmpty(i)) throw new EmptyStackException();
+            System.out.print(i+1+" 번째 stack : ");
+            int limit = this.top[i] + i*this.len;
+            for (int j = 0; j < this.len; j++){
+                int index = i * this.len + j;
+                if (limit >= index) {
+                    System.out.print(stackAry[index]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+// 2.2 유동길이 : 배열을 이용하여 여러개의 Stack을 구현한다.
+//              stack이 공간을 모두 사용하였을 경우에는 다른 stack의 공간을 사용한다.
+//              이때, 배열을 하나의 원으로 생각하여 공간을 비려준다. 그러나 실제로는 일직선이므로 Mod를 사용하여 할당한다.
+class StackFlowArray{
+
+    class Stack{
+        int top;
+        int len;
+        int start;
+
+        Stack(int len, int start){
+            this.top = -1;
+            this.len = len;
+            this.start = start;
+        }
+
+
 
     }
 }
@@ -105,5 +153,38 @@ public class Stack_Basic {
         System.out.println(stackList.peek());
         System.out.println(stackList.pop());
         stackList.retrieve();
+
+        StackFixArray stackFixArray = new StackFixArray(5,3);
+        try {
+            for (int j = 0; j < 3; j++){
+                for (int i = 0; i < 5; i++){
+                    stackFixArray.push(j,new Random().nextInt(10));
+                }
+            }
+        } catch (FullStackException e) {
+            e.printStackTrace();
+        }
+
+        stackFixArray.retireve();
+
+        System.out.println(stackFixArray.pop(0));
+        System.out.println(stackFixArray.pop(0));
+        System.out.println(stackFixArray.peek(0));
+        System.out.println(stackFixArray.pop(0));
+        System.out.println(stackFixArray.isEmpty(0));
+        System.out.println(stackFixArray.pop(0));
+        System.out.println(stackFixArray.pop(0));
+        System.out.println(stackFixArray.isEmpty(0));
+
+        try {
+            stackFixArray.push(0,new Random().nextInt(10));
+        } catch (FullStackException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(stackFixArray.pop(2));
+
+        stackFixArray.retireve();
+
     }
 }
