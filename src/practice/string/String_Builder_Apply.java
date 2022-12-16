a0ecdeb0f821575716e09b4c6125f171802b7e9d
김소영 <soyoungdev.7@gmail.com>
2022-12-17 00:27:11 +0900

    221217_Commit

--- src/practice/string/String_Builder_Apply.java
+++ src/practice/string/String_Builder_Apply.java
@@ -1,6 +1,36 @@
 package practice.string;
+/**
+ * StringBuilder 응용
+ * 문자열 압출(String Compression)
+ * : 반복되는 문자를 숫자로 압축해서 반환하는 알고리즘
+ * => String은 값을 수정하는 과정에서 계속적으로 순회가 일어나기 때문에 비효율적이다.
+ * => StringBuilder은 값을 수정하는 과정에서는 효율적이지만,
+ *    공간 부족시에  Doubling작업을하기 때문에 비효율적이다.
+ * => StringBuilder하는 동시에 문자열의 길이를 사전에 미리 전달하면,
+ *    공간부족을 줄이기 때문에 Doubling작업의 비효율을 줄일 수 있다.
+ */
 
 public class String_Builder_Apply {
+
+    private static String stringCompression(String str) {
+        String newStr = compress(str);
+        // 반복이 없는 문자열은 바로 출력
+        return str.length() < newStr.length() ? str : newStr;
+    }
+
+    private static String compress(String str) {
+        int count = 0;
+        StringBuilder strBuilder = new StringBuilder(getTotal(str));
+        for (int i = 0; i < str.length(); i++) {
+            count ++;
+        }
+        return null;
+    }
+
+    private static int getTotal(String str) {
+        return 0;
+    }
+
     public static void main(String[] args) {
 
     }
--- src/practice/string/String_Builder_Basic.java
+++ src/practice/string/String_Builder_Basic.java
@@ -1,7 +1,43 @@
 package practice.string;
+/**
+ * StringBuilder
+ */
+class StringBuilderBasic{
+    private int size;
+    private int index;
+    private char[] value;
+
+    StringBuilderBasic(){
+        this.size = 1;
+        this.index = 0;
+        this.value = new char[this.size];
+    }
+
+    public void append(String str) {
+        if (str == null) return;
+        strCapacity(str.length());
+        for (int i = 0; i < str.length(); i++) value[index++] = str.toCharArray()[i];
+    }
+
+    private void strCapacity(int len) {
+        if (len == 0) return;
+        this.size = this.size + len; // ArrayList와 달리 문자의 길이 만큼 확장한다.
+        char[] newVal = new char[this.size];
+        for (int i = 0; i < this.value.length; i++) newVal[i] = this.value[i];
+        this.value = newVal;
+        System.out.println(this.size);
+    }
+
+    public String toString(){return new String(this.value,0,index);}
+
+}
 
 public class String_Builder_Basic {
     public static void main(String[] args) {
-
+        StringBuilderBasic str = new StringBuilderBasic();
+        str.append("StringBuilder");
+        str.append(" is");
+        str.append(" Good");
+        System.out.println(str.toString());
     }
 }
